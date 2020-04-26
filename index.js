27794fa4e98d1e1879a2c8f1db5768fc3140e2a8
Zander Olidan <zander@playstudios.asia>
2020-04-26 21:04:11 +0800

    Added rate limiting

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ require('dotenv').config();
 const Telegraf = require('telegraf');
 const Markup = Telegraf.Markup;
 const LocalSession = require('telegraf-session-local');
+const RateLimit = require('telegraf-ratelimit')
 // @ts-ignore
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
+// Set limit to 1 message per 3 seconds
+const limitConfig = {
+    window: 3000,
+    limit: 1,
+    onLimitExceeded: (ctx, next) => {},
+  }
 
 const localSession = new LocalSession({
     getSessionKey: (ctx) => {
@@ -24,7 +32,7 @@ const hasAdded = async (ctx) => {
     return ctx.session.users && ctx.session.users.findIndex(user => user.id === ctx.from.id) > -1;
 };
 
-
+bot.use(RateLimit(limitConfig))
 bot.use(localSession.middleware())
 bot.command('pingmoko', async (ctx, next) => {
     if (await hasAdded(ctx)) {
@@ -60,7 +68,7 @@ bot.command('yokoping', async (ctx, next) => {
         newList.splice(userIndex, 1);
         ctx.session.users = [...newList];
 
-        ctx.reply(`Yeeeeeeeeted ${ctx.from.first_name}`);
+        ctx.reply(`Yeeeeeeeeted ${ctx.from.first_name} from the annoy list`);
         return next();
     }
 
